7418293353f2639cc8f43eba87bb8e9f393109ba
Keith Green <kegreen22@gmail.com>
2015-04-20 19:35:23 -0400

    4-20-15 created users & home controllers, updated gem file, created migrations

---
+++ db/migrate/20150420231259_add_interests_to_users.rb
@@ -0,0 +1,15 @@
+class AddInterestsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zipcode, :string
+    add_column :users, :interest1, :string
+    add_column :users, :interest2, :string
+    add_column :users, :interest3, :string
+
+    add_column :users, :password_digest, :string
+    add_column :users, :slug, :string
+
+  end
+end
