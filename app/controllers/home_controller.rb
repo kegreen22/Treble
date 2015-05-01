190b61ecf3dc035d0d760fa0c4abd8846a13e0f9
Keith Green <kegreen22@gmail.com>
2015-05-01 19:52:44 -0400

    5-1-15 added weather api & modded input form to include interests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,9 +11,9 @@ class HomeController < ApplicationController
   @zipcode = User.zipcode
   # call api methods and insert interest into api call
 
-  nytimes
-  meetup
-  indeed
+  @nytimes_final = nytimes
+  @meetup_final = meetup
+  weather_rpt
 
 # view displays the api results
 
@@ -41,36 +41,41 @@ class HomeController < ApplicationController
   private
 
   def nytimes
-  # insert api into nytimes api call
-
-
-  url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?&q=" + @interest1 "+" + @interest2 + "&sort=newest&api-key=bd2d3da37f58e2247ab30155400fc222:3:67128716"
-  nyt_data = open(url).read
-  # now parse and put into an array (shown below)
-  nyt_result = JSON.parse(groups_data)
+    # insert api into nytimes api call
+    url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?&q=" + @interest1 "+" + @interest2 + "&sort=newest&api-key=bd2d3da37f58e2247ab30155400fc222:3:67128716"
+    nyt_data = open(url).read
+    # now parse and put into an array (shown below)
+    nyt_result = JSON.parse(nyt_data)
   
   end
 
 
   def meetup
-  # insert api into meetup api call
-  url = "https://api.meetup.com/2/open_events.zip=" + self.zipcode + "topic=" + @interest1 + "&time=,1w&key="
-  meetup_data = open(url).read
-  # now parse and put into an array (shown below)
-  meetup_result = JSON.parse(groups_data)
+    # insert api into meetup api call
+    url = "https://api.meetup.com/2/open_events.zip=" + self.zipcode + "&text=" + @interest1 + "+" + @interest2 + "&time=,1m&key=6874237675483c4f5e12f416939655a"
+    meetup_data = open(url).read
+    # now parse and put into an array (shown below)
+    meetup_result = JSON.parse(meetup_data)
 
   end
 
 
   def indeed
-  # insert api into meetup api call
-  url = "http://api.indeed.com/ads/apisearch?publisher=3117837613642246&q=" + @interest1 + "+" @interest2 + "&l=" + @zipcode + 
-  "&sort=&radius=5&st=&jt=&start=&limit=50&fromage=15&filter=&latlong=0&co=us&chnl=&userip=" + machine ip + "&useragent=" + browser + "&v=2"
+    # insert api into meetup api call
+    # url = "http://api.indeed.com/ads/apisearch?publisher=3117837613642246&q=" + @interest1 + "+" @interest2 + "&l=" + @zipcode + 
+    #{ }"&sort=&radius=5&st=&jt=&start=&limit=50&fromage=15&filter=&latlong=0&co=us&chnl=&userip=" + machine ip + "&useragent=" + browser + "&v=2"
 
-  indeed_data = open(url).read
-  # now parse and put into an array (shown below)
-  groups_result = XML.parse(groups_data) # check what is the correct method to parse to XML
+    # indeed_data = open(url).read
+    # now parse and put into an array (shown below)
+    # groups_result = XML.parse(groups_data) # check what is the correct method to parse to XML
   end
+
+  def weather_rpt
+    url = "http://api.wunderground.com/api/cfffe9ffeb7b662e/conditions/q/" + User.state + "/" + User.city + ".json"
+    weather_data = open(url).read
+    weather_result = JSON.parse(weather_data)
+  end
+
 
   # end of class
   
