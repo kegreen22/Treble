44f5419e5baed0c7989ccd0ff4f5c6c229cd0645
Keith Green <kegreen22@gmail.com>
2015-05-07 18:47:37 -0400

    5-7-15 added css, tested & ran specs for model, added home pg

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,22 +1,23 @@
 require 'rails_helper'
  
+describe User do
 # test no username
 it "is invalid without a username" do
   john_doe = User.new(username: nil, password: 'imminent')
   john_doe.valid?
-  expect(john_doe.errors[:username]).to include("Username can't be blank")
+  expect(john_doe.errors[:username]).to include("can't be blank")
 end
+
  
 # test no password
 it "is invalid without a password" do
   john_doe = User.new(username: 'JohnD', password: nil)
   john_doe.valid?
-  expect(john_doe.errors[:password]).to include("Password can't be blank")
+  expect(john_doe.errors[:password]).to include("can't be blank")
 end
  
 # test valid with username, password, interest1, interest2
-describe New_user do
-  it "is valid with a username, password and interests" do
+it "is valid with a username, password and interests" do
   s_richards = User.new(
     username: 'SRichards',
     password: 'invisible',
@@ -24,7 +25,7 @@ describe New_user do
     interest2: 'amazon')
   expect(s_richards).to be_valid
 end
-end
+
  
 # test username uniqueness
   it "is invalid with a duplicate username" do
@@ -36,8 +37,8 @@ end
          username: 'JaneT',
          password: 'dragon2'
        )
-       janet.valid?
-       expect(janet.errors[:username]).to include("Username has already been taken")
+       janet2.valid?
+       expect(janet2.errors[:username]).to include("has already been taken")
   end
  
  
@@ -45,6 +46,6 @@ end
 it "is invalid with a password shorter than 5 characters" do
   john_doe = User.new(username: 'JohnD', password: 'tom', interest1: 'shadow', interest2: 'cat')
   john_doe.valid?
-  expect(john_doe.errors[:password]).to include("Password is too short (minimum is 5 characters)")
+  expect(john_doe.errors[:password]).to include("is too short (minimum is 5 characters)")
 end
- 
+end # end of initial describe statement
