e3faebd2ac30fc3763618c2d7aedf5b6958d1a22
Keith Green <kegreen22@gmail.com>
2015-05-24 23:57:42 -0400

    5-24-15 added view logic for top stories & articles to iterate thru json data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -79,6 +79,8 @@ class HomeController < ApplicationController
   # @weather_fnextdaytext = []
 end
 
+  @test = 0 # use this to show other tabs; if it is = 1 then other tabs will be shown
+
   top_stories
   articles
   networking
@@ -97,13 +99,13 @@ end
   
   
   def top_stories   # Top stories
-   
+   # check for useful results 
+  if ((@nytimes_top["status"] != "OK") || (@nytimes_top["num_results"].to_i == 0))
+  @top_flag == 0
+  else
+  @top_flag = 1
     @top_title = @nytimes_top["results"]["title"] # with title being the link_to to the 'url'; 
     
-    if @top_title.blank?
-      @top_title = "Unable to retrieve top stories at this time" 
-      @top_url, @top_date = "No information at this time"
-    else
     @top_url = @nytimes_top["results"]["url"]
     @top_date = @nytimes_top["results"]["last_updated"]
   end
@@ -111,43 +113,50 @@ end
 
 
   def articles    # Article search
-    @headline = @nytimes_data["response"]["docs"]["headline"]["main"]
-    if @headline.blank? 
-     @headline = "No news on your interests at this time" # test if there is data
-     @art_date, @art_url = "No information at this time"
+   # check for useful results 
+  if ((@nytimes_data["status"] != "OK"]) || (@nytimes_data["response"]["meta"]["hits"]).to_i == 0)) 
+  @article_flag = 0
   else
-    @art_date = @nytimes_data["response"]["docs"]["pub_date"]
-    @art_url = @nytimes_data["response"]["docs"]["web_url"]
-  end
+  @article_flag = 1
+
+
+    #@headline = @nytimes_data["response"]["docs"] #["headline"]["main"]
+    #if @headline.blank? 
+    # @headline = "No news on your interests at this time" # test if there is data
+    # @art_date, @art_url = "No information at this time"
+  #else
+   # @art_date = @nytimes_data["response"]["docs"] #["pub_date"]
+   # @art_url = @nytimes_data["response"]["docs"] #["web_url"]
+  #end
 end
 
   def networking
-    @meetup_grpname = @meetup_data["results"]["group"]["name"]
-    if @meetup_grpname.blank?
-      @meetup_grpname = "No information on meetup groups" 
-      @meetup_locname, @meetup_address, @meetup_city, @meetup_desc, @meetup_url, @meetup_status = "No information at this time"
-    else
-    @meetup_locname = @meetup_data["results"]["venue"]["name"]
-    @meetup_address = @meetup_data["results"]["venue"]["address1"]
-    @meetup_city = @meetup_data["results"]["venue"]["city"]
-    @meetup_desc = @meetup_data["results"]["name"]
-    @meetup_url = @meetup_data["results"]["event_url"]
-    @meetup_status = @meetup_data["results"]["status"]
+#    @meetup_grpname = @meetup_data["results"]["group"]["name"]
+ #   if @meetup_grpname.blank?
+  #    @meetup_grpname = "No information on meetup groups" 
+   #   @meetup_locname, @meetup_address, @meetup_city, @meetup_desc, @meetup_url, @meetup_status = "No information at this time"
+    #else
+    #@meetup_locname = @meetup_data["results"]["venue"]["name"]
+    #@meetup_address = @meetup_data["results"]["venue"]["address1"]
+    #@meetup_city = @meetup_data["results"]["venue"]["city"]
+    #@meetup_desc = @meetup_data["results"]["name"]
+    #@meetup_url = @meetup_data["results"]["event_url"]
+    #@meetup_status = @meetup_data["results"]["status"]
 
-  end
+#  end
 end
 
   def events   # Events
-    @events_desc = @nytimes_events["results"]["web_description"]  # description of event
-    if @events_desc.blank?
-      @events_desc = "No information on local events meeting your free time interest"
-      @events_name, @events_url, @events_free = "No information at this time"
-    else
-    @events_name = @nytimes_events["results"]["event_name"]
+ #   @events_desc = @nytimes_events["results"] #["web_description"]  # description of event
+  #  if @events_desc.blank?
+   #   @events_desc = "No information on local events meeting your free time interest"
+    #  @events_name, @events_url, @events_free = "No information at this time"
+    #else
+    #@events_name = @nytimes_events["results"]["event_name"]
     # @events_date = @nytimes_events["results"][""]
-    @events_url = @nytimes_events["results"]["event_detail_url"]
-    @events_free = @nytimes_events["results"]["free"]
-  end
+   # @events_url = @nytimes_events["results"]["event_detail_url"]
+   # @events_free = @nytimes_events["results"]["free"]
+ # end
 end
 
   def data_retrieve(url_string)
@@ -155,67 +164,12 @@ end
     data_read =open(url).read
     @data_result = JSON.parse(data_read)
 
-    unless @data_result.blank?
-   puts "search good" 
-    end
-  return @data_result
+    return @data_result
   end
 
 
 
-  private
-
-  def nytimes
-    # insert api into nytimes api call
-    # url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?&q=" + @interest1 "+" + @interest2 + "&sort=newest&api-key=bd2d3da37f58e2247ab30155400fc222:3:67128716"
-    # nyt_data = open(url).read
-    # now parse and put into an array (shown below)
-    # nyt_result = JSON.parse(nyt_data)
  
-  end
-
-
-  def meetup
-    # insert api into meetup api call
-    # url = "https://api.meetup.com/2/open_events.zip=" + self.zipcode + "&text=" + @interest1 + "+" + @interest2 + "&time=,1m&key=6874237675483c4f5e12f416939655a"
-    # meetup_data = open(url).read
-    # now parse and put into an array (shown below)
-    # meetup_result = JSON.parse(meetup_data)
-
-  end
-
-
-  def indeed
-    # insert api into meetup api call
-    # url = "http://api.indeed.com/ads/apisearch?publisher=3117837613642246&q=" + @interest1 + "+" @interest2 + "&l=" + @zipcode + 
-    #{ }"&sort=&radius=5&st=&jt=&start=&limit=50&fromage=15&filter=&latlong=0&co=us&chnl=&userip=" + machine ip + "&useragent=" + browser + "&v=2"
-
-    # indeed_data = open(url).read
-    # now parse and put into an array (shown below)
-    # groups_result = XML.parse(groups_data) # check what is the correct method to parse to XML
-  end
-
-  def weather_rpt
-    # @user_state = User.state
-    # @user_city = User.city
-    # url = "http://api.wunderground.com/api/cfffe9ffeb7b662e/conditions/q/" + User.state + "/" + User.city + ".json"
-    # weather_data = open(url).read
-    # weather_result = JSON.parse(weather_data)
-  end
-
-  def freetime
-    # @user.zipcode = User.zipcode
-    # url = 
-    # freetime_data = open(url).read
-    # freetime_events = JSON.parse(freetime_data)
-  end
-
-  def top_stories
-    # url = 
-    # top_data = open(url).read
-    # top_info = JSON.parse(top_data)
-  end
-
 
   # end of class
   
