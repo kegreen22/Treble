1fb8722c906a33f1bef29192006395c10614d283
Keith Green <kegreen22@gmail.com>
2015-06-03 14:08:31 -0400

    6-3-15 rmv caching; edited comments; added description of interest and free time for user on their home pg

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,27 +9,24 @@ class HomeController < ApplicationController
   # get user interest from the database
   @user = current_user
   @interest_pre = @user.interest1
-  #puts @user.slug
-  #puts @user.interest1
+ 
   @interest_pre = @interest_pre.gsub(/\s+/,'+') # add + symbol between each word if there are 2+ words to allow use in http searches - designates "or" in the article searches
-  #puts @interest_pre
+ 
   @zipcode = @user.zipcode
-  #puts current_user.zipcode
-
+  
   @free_pre = @user.free_time  
   @free_pre = @free_pre.gsub(/\s+/,'+') # add + symbol between each word if there are 2+ words to allow use in http searches - designates "or" in the event searches
-  # puts @free_pre 
+  
 
   @state_pre = @user.state.gsub(/\s+/,'_')
   @city_pre = @user.city.gsub(/\s+/,'_')
-  # puts @city_pre
-  # call api methods and insert interest into api call
+  
 
   @testing = Date.today
   @limit = @testing.days_ago(7)
   @begin_time = @limit.to_s
   @begin_time = @begin_time.gsub('-','')
-  #puts @begin_time
+ 
 
   @nytimes_data = data_retrieve("http://api.nytimes.com/svc/search/v2/articlesearch.json?&q=" + @interest_pre + "&begin_date=" + @begin_time + "&sort=newest&api-key=bd2d3da37f58e2247ab30155400fc222:3:67128716")
   @weather_rpt = data_retrieve("http://api.wunderground.com/api/cfffe9ffeb7b662e/conditions/q/" + @state_pre + "/" + @city_pre + ".json")
@@ -39,7 +36,7 @@ class HomeController < ApplicationController
   @nytimes_events = data_retrieve("http://api.nytimes.com/svc/events/v2/listings.json?&query=" + @free_pre + "&limit=20&api-key=3484b827fcc7f7a5962abbf4b36fdfc4:19:67128716")
 
    
-  # determine how many results were returned
+  # determine how many results were returned and rescue if there is an error
   begin
   @hits = (@nytimes_data["response"]["meta"]["hits"]).to_i # article search hits
     rescue NoMethodError => e
@@ -70,11 +67,6 @@ class HomeController < ApplicationController
     @weather_rpt = {}
   end
 
-
-  # see the current temperature
-  #@weather_current = @weather_rpt['current_observation']['temp_f']
-  
-
 else
   render 'index'
 
@@ -85,8 +77,6 @@ end
 end
 
   # assign json array info to variables
-  
-  
   def data_retrieve(url_string)
     url = url_string  # open url 
     
